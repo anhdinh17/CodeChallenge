817f6b52715f054ad2307943713cb2b896f279b4
Alex Dinh <anhdinh17@gmail.com>
2021-03-13 14:57:29 -0600

    Finished TableView and Cell

---
+++ CodeChallenge/CustomTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  TableViewCell.swift
+//  CodeChallenge
+//
+//  Created by Anh Dinh on 3/13/21.
+//
+
+import UIKit
+import SnapKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "test_cell_identifier"
+    
+    var cellLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.frame = CGRect(x:165,
+                             y: 10,
+                             width: 100,
+                             height: 50)
+        label.font = UIFont(name: "Helvetica-Bold", size: 18)
+        return label
+    }()
+    
+    var cellType: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.frame = CGRect(x:165,
+                             y: 40,
+                             width: 100,
+                             height: 50)
+        label.font = UIFont(name: "Helvetica", size: 17)
+        return label
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.contentView.addSubview(cellLabel)
+        self.contentView.addSubview(cellType)
+//        cellLabel.snp.makeConstraints {
+//            $0.left.equalToSuperview().offset(10)
+//            $0.right.equalToSuperview().offset(-10)
+//            $0.top.equalToSuperview().offset(50)
+//            $0.bottom.equalToSuperview().offset(-50)
+//        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- CodeChallenge/ViewController.swift
+++ CodeChallenge/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var array = ["Hello Kitty", "Saturday", "Monday"]
+    
+    // TableView
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.rowHeight = 100
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        // register CustomTableViewCell
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identifier)
+        // Using Snapkit to make constraints for TableView
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self.view)
+        }
+        
     }
 
+//MARK: - TableView DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier, for: indexPath) as! CustomTableViewCell
+      //  cell.textLabel?.text = array[indexPath.row]
+        
+//        cell.contentView.addSubview(cell.cellLabel)
+//        cell.contentView.addSubview(cell.cellType)
+        
+        
+//
+        cell.cellLabel.text = array[indexPath.row]
+        cell.cellType.text = array[indexPath.row]
+        return cell
+    }
 
+//MARK: - TableView Delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
